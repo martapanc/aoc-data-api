040229cff5a10e6972163f98b11c980066631026
martapanc <marta.panc@gmail.com>
2021-01-16 18:06:02 +0000

    Add HttpStatusException handling

--- src/main/kotlin/com/workspace/aocdataapi/controller/StarsController.kt
+++ src/main/kotlin/com/workspace/aocdataapi/controller/StarsController.kt
@@ -1,9 +1,10 @@
 package com.workspace.aocdataapi.controller
 
-import com.workspace.aocdataapi.service.AoCEvent
 import com.workspace.aocdataapi.service.Session
 import com.workspace.aocdataapi.service.retrieveStarsService
+import org.jsoup.HttpStatusException
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,8 +15,15 @@ class StarsController {
     lateinit var session: Session
 
     @GetMapping("/stars")
-    fun retrieveStars(): List<AoCEvent> {
+    fun retrieveStars(): ResponseEntity<String> {
         val sessionCookie: String = session.sessionCookie
-        return retrieveStarsService(sessionCookie)
+        return try {
+            val list = retrieveStarsService(sessionCookie)
+            ResponseEntity.ok(list.toString())
+        } catch (e: HttpStatusException) {
+            ResponseEntity.status(504).body("Error 504 connecting to https://adventofcode.com/")
+        } catch (e: Exception) {
+            ResponseEntity.status(500).body("Error connecting to https://adventofcode.com/")
+        }
     }
 }
--- src/main/kotlin/com/workspace/aocdataapi/service/StarService.kt
+++ src/main/kotlin/com/workspace/aocdataapi/service/StarService.kt
@@ -14,17 +14,21 @@ class Session {
 
 fun retrieveStarsService(session: String): List<AoCEvent> {
     val cookie = mapOf("session" to session)
-    val document = Jsoup.connect("https://adventofcode.com/events").cookies(cookie).get()
-    val eventListDivs = document.getElementsByClass("eventlist-event")
+    try {
+        val document = Jsoup.connect("https://adventofcode.com/events").cookies(cookie).get()
+        val eventListDivs = document.getElementsByClass("eventlist-event")
 
-    val events = mutableListOf<AoCEvent>()
-    for (div in eventListDivs) {
-        val anchor = div.getElementsByTag("a")[0]
-        val year = anchor.text().substring(1, 5).toInt()
-        val span = div.getElementsByTag("span")
-        val stars = if (span.size > 0) span[0].text().replace("*", "").toInt() else 0
-        events.add(AoCEvent(year, stars))
+        val events = mutableListOf<AoCEvent>()
+        for (div in eventListDivs) {
+            val anchor = div.getElementsByTag("a")[0]
+            val year = anchor.text().substring(1, 5).toInt()
+            val span = div.getElementsByTag("span")
+            val stars = if (span.size > 0) span[0].text().replace("*", "").toInt() else 0
+            events.add(AoCEvent(year, stars))
+        }
+
+        return events
+    } catch (e: Exception) {
+        throw e
     }
-
-    return events
 }

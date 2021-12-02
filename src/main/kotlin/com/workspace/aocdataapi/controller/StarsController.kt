a41fb85e3a998c6f4e114158c036ff0cfcba2906
martapanc <marta.panc@gmail.com>
2021-12-02 14:29:35 +0000

    Handle session cookie expired error

--- src/main/kotlin/com/workspace/aocdataapi/controller/StarsController.kt
+++ src/main/kotlin/com/workspace/aocdataapi/controller/StarsController.kt
@@ -4,8 +4,11 @@ import com.workspace.aocdataapi.service.AoCEvent
 import com.workspace.aocdataapi.service.Session
 import com.workspace.aocdataapi.service.retrieveStarsService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpClientErrorException
 
 @RestController
 class StarsController {
@@ -16,6 +19,14 @@ class StarsController {
     @GetMapping("/stars")
     fun retrieveStars(): List<AoCEvent> {
         val sessionCookie: String = session.sessionCookie
-        return retrieveStarsService(sessionCookie)
+        val eventList: List<AoCEvent> = retrieveStarsService(sessionCookie)
+        if (eventList.isNotEmpty()) {
+            return eventList
+        } else {
+            throw HttpClientErrorException(
+                HttpStatus.BAD_REQUEST,
+                "Could not retrieve stars - maybe the session cookie has expired?"
+            )
+        }
     }
 }
--- src/main/kotlin/com/workspace/aocdataapi/service/StarService.kt
+++ src/main/kotlin/com/workspace/aocdataapi/service/StarService.kt
@@ -18,10 +18,16 @@ fun retrieveStarsService(session: String): List<AoCEvent> {
     val eventListDivs = document.getElementsByClass("eventlist-event")
 
     val events = mutableListOf<AoCEvent>()
+
+    // If the user is not logged in, the `eventlist-event` div only contains the link to the year's event
+    if (eventListDivs[0].childNodeSize() < 5) {
+        return events
+    }
+
     for (div in eventListDivs) {
         val anchor = div.getElementsByTag("a")[0]
         val year = anchor.text().substring(1, 5).toInt()
-        val span = div.getElementsByTag("span")
+        val span = div.getElementsByClass("star-count")
         val stars = if (span.size > 0) span[0].text().replace("*", "").toInt() else 0
         events.add(AoCEvent(year, stars))
     }
